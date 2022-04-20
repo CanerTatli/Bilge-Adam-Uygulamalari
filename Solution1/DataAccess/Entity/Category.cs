b3364ac4106f1f0d2afa3bcafea691f258b05306
Ahmet Caner Tatlı <ahmet.tatli_66@hotmail.com>
2022-04-20 13:22:53 +0300

    Db oluşturuldu-Entityler hazır.

--- Solution1/DataAccess/Entity/Category.cs
+++ Solution1/DataAccess/Entity/Category.cs
@@ -10,6 +10,8 @@ namespace DataAccess.Entity
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        
+
+        public List<Product> Products { get; set; }
+
     }
 }
--- Solution1/DataAccess/Entity/Product.cs
+++ Solution1/DataAccess/Entity/Product.cs
@@ -13,6 +13,11 @@ namespace DataAccess.Entity
         public decimal UnitPrice { get; set; }
         public int UnitsInStock { get; set; }
 
+
+
+        public int CategoryId { get; set; }
+        
+        public Category Category { get; set; }
         
 
     }
