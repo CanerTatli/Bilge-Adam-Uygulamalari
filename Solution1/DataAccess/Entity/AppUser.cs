1c46b5595634d7ece3616fe2b11ccf7660bdb1b3
Ahmet Caner Tatlı <ahmet.tatli_66@hotmail.com>
2022-04-20 13:04:24 +0300

    Entity'ler oluşturuluyor - commit deneme

---
+++ Solution1/DataAccess/Entity/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entity
+{
+    internal class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        public char Password { get; set; }
+
+
+
+
+
+    }
+}
