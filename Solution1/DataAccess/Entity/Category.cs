8bfa32cfa3060bd58bbad096c6fbd9c581d84560
Ahmet Caner Tatlı <ahmet.tatli_66@hotmail.com>
2022-04-20 13:37:21 +0300

    Bll ve UI oluşturuldu.

--- Solution1/DataAccess/Entity/Category.cs
+++ Solution1/DataAccess/Entity/Category.cs
@@ -9,7 +9,7 @@ namespace DataAccess.Entity
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string CategoryName { get; set; }
 
         public List<Product> Products { get; set; }
 
