c3ee55c5dbbd9785d005acf85e8acce7da81ce42
CanerTatli <101792057+CanerTatli@users.noreply.github.com>
2022-04-20 12:39:22 +0300

    Add files via upload

---
+++ Solution1/DataAccess/Context/Westwind.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    internal class Westwind:DbContext
+    {
+        public Westwind()
+        {
+            Database.Connection.ConnectionString = "Server=DESKTOP-S3GJTPT\\SQLEXPRESS;Database=Westwind;Trusted_Connection=True;";
+
+
+
+
+
+        }
+
+    }
+}
